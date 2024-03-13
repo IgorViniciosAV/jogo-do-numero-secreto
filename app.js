ffde3ee94d605aa36110f678d327681d3b18323c
IgorViniciosAV <159195982+IgorViniciosAV@users.noreply.github.com>
2024-03-12 21:06:02 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ document.addEventListener("keydown", function(e) {
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    // responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
 function exibirMensagemInicial() {
